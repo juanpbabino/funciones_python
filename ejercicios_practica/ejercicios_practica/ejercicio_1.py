f93428c8c16a88048fb0f72622115cf313bdac4b
juanpbabino <87047759+juanpbabino@users.noreply.github.com>
2021-08-17 07:47:16 -0300

    Ejercicios de Practica realizados
    Ejercicios de Practica - Funciones, realizados.

---
+++ ejercicios_practica/ejercicios_practica/ejercicio_1.py
@@ -0,0 +1,34 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones
+
+def imprimir_mayor(numero_1, numero_2):
+    print("Función imprimir mayor")
+    if max(numero_1,numero_2):
+       print("numero {} es mayor a numero {}".format(numero_2,numero_1))
+    elif numero_1 < numero_2:
+        print("numero {} es mayor a numero {}".format(numero_1,numero_2))
+    else:
+        print("numero 1 es igual numero 2")
+    
+    
+    # En esta función debe determinar cual de los dos
+    # números ingresados por parámetro es mayor
+    # y luego imprimir dicho valor en pantalla
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    # Alumno: Complete la función "imprimir_mayor"
+    
+    imprimir_mayor(2, 10)
+    
+    print("terminamos")
---
+++ ejercicios_practica/ejercicios_practica/ejercicio_5.py
@@ -0,0 +1,66 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones y módulos
+import random
+lista_numeros = []
+# --------------------------------
+# Aquí dentro definir la función contar
+def contar (lista_numeros, fin):
+    cantidad_tres = lista_numeros.count(fin)
+    return cantidad_tres
+
+
+# Aquí copiar la función lista_aleatoria
+# ya elaborada
+def lista_aleatoria (inicio, fin, cantidad): 
+    for numero in range(cantidad):
+        numero = random.randrange(inicio, fin+1)
+        lista_numeros.append(numero)
+
+# --------------------------------
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    inicio = 0
+    fin = 9
+    cantidad = 5
+
+    # Alumno: Crear la función "contar"
+
+    # Utilice la función "lista_aleatoria"  creado antes 
+    # para generar una lista de 5 números en
+    # un rango de 1 a 9 inclusive
+
+    # lista_numeros = lista_aleatoria(inicio, fin, cantidad)
+
+    # Generar una una nueva funcion que se llame "contar",
+    # que cuente la cantidad de veces que un elemento pasado
+    # por parámetro se repite en la lista también pasada por parámetro
+    
+    # Para saber cuantas veces se repiten el elemento pasado
+    # en la lista pueden usar el método nativo de list "count"
+
+    # Por ejemplo creo una lista de 5 elemtnos
+    
+    # Luego quiero averiguar cuantas veces se repite el numero 3
+    # cantidad_tres = contar(lista_numeros, 3)
+
+    # Luego de crear la función invocarla en este lugar:
+    # Averiguar cuantas veces se repite el numero 3
+
+    # cantidad_tres = contar(lista_numeros, 3)
+    cantidad_tres = contar(lista_numeros, 3)
+    # Imprimir en pantalla "cantidad_tres" que informa
+    # cuantas veces se repite el tres en la lista
+    # print(cantidad_tres)
+    print("Cuantas veces se repite el numero en la lista", cantidad_tres)
+    print("terminamos")
